4b61fd031fe3a0f7e9c40c261fe095849e8cbdff
JAMNESIA <37365193+JAMNESIA@users.noreply.github.com>
2018-03-21 09:41:56 +0100

    Main menu
    Main menu gemaakt

---
+++ Data/Data/Datavisualisatie1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Data
+{
+    public partial class Datavisualisatie1 : Form
+    {
+        public Datavisualisatie1()
+        {
+            InitializeComponent();
+        }
+
+        private void myButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 mainMenu = new Form1();
+            mainMenu.Show();
+        }
+    }
+}
--- Data/Data/Form1.cs
+++ Data/Data/Form1.cs
@@ -16,5 +16,38 @@ namespace Data
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Datavisualisatie1 Datavisualisatie1 = new Datavisualisatie1();
+            Datavisualisatie1.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Datavisualisatie2 Datavisualisatie2 = new Datavisualisatie2();
+            Datavisualisatie2.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Datavisualisatie3 Datavisualisatie3 = new Datavisualisatie3();
+            Datavisualisatie3.Show();
+        }
+
+        private void Settings_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Settings Settings = new Settings();
+            Settings.Show();
+        }
+
+        private void Shutdown_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
