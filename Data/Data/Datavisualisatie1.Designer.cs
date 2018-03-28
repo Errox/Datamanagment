aa10b0f23000b43de0cbddfb3aea8219b2a9d544
MartijnNieuwenhuis <36473925+MartijnNieuwenhuis@users.noreply.github.com>
2018-03-28 10:29:04 +0200

    proberen bugs te fixen

--- Data/Data/Datavisualisatie1.Designer.cs
+++ Data/Data/Datavisualisatie1.Designer.cs
@@ -36,9 +36,6 @@ namespace Data
         {
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Datavisualisatie1));
             this.myButton = new System.Windows.Forms.Button();
             this.WFDChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
@@ -65,7 +62,7 @@ namespace Data
             // myButton
             // 
             this.myButton.Location = new System.Drawing.Point(1011, 670);
-            this.myButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.myButton.Margin = new System.Windows.Forms.Padding(4);
             this.myButton.Name = "myButton";
             this.myButton.Size = new System.Drawing.Size(167, 28);
             this.myButton.TabIndex = 0;
@@ -75,29 +72,17 @@ namespace Data
             // 
             // WFDChart
             // 
+            this.WFDChart.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
             chartArea1.Name = "ChartArea1";
             this.WFDChart.ChartAreas.Add(chartArea1);
             legend1.Name = "Legend1";
             this.WFDChart.Legends.Add(legend1);
             this.WFDChart.Location = new System.Drawing.Point(712, 153);
-            this.WFDChart.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.WFDChart.Margin = new System.Windows.Forms.Padding(4);
             this.WFDChart.Name = "WFDChart";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Min. Temperatuur";
-            series2.ChartArea = "ChartArea1";
-            series2.Legend = "Legend1";
-            series2.Name = "Max. Temperatuur";
-            series3.ChartArea = "ChartArea1";
-            series3.Legend = "Legend1";
-            series3.Name = "Gestolen fietsen";
-            this.WFDChart.Series.Add(series1);
-            this.WFDChart.Series.Add(series2);
-            this.WFDChart.Series.Add(series3);
             this.WFDChart.Size = new System.Drawing.Size(831, 492);
             this.WFDChart.TabIndex = 30;
             this.WFDChart.Text = "Windsnelheid en geboorte en sterfte";
-            this.WFDChart.Click += new System.EventHandler(this.WGSchart_Click);
             // 
             // WGSmaand
             // 
@@ -114,9 +99,9 @@ namespace Data
             this.WGSmaand.Controls.Add(this.WGSfeb);
             this.WGSmaand.Controls.Add(this.WGSjan);
             this.WGSmaand.Location = new System.Drawing.Point(124, 453);
-            this.WGSmaand.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.WGSmaand.Margin = new System.Windows.Forms.Padding(4);
             this.WGSmaand.Name = "WGSmaand";
-            this.WGSmaand.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.WGSmaand.Padding = new System.Windows.Forms.Padding(4);
             this.WGSmaand.Size = new System.Drawing.Size(533, 160);
             this.WGSmaand.TabIndex = 27;
             this.WGSmaand.TabStop = false;
@@ -125,8 +110,10 @@ namespace Data
             // WGSdec
             // 
             this.WGSdec.AutoSize = true;
+            this.WGSdec.Checked = true;
+            this.WGSdec.CheckState = System.Windows.Forms.CheckState.Checked;
             this.WGSdec.Location = new System.Drawing.Point(384, 96);
-            this.WGSdec.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.WGSdec.Margin = new System.Windows.Forms.Padding(4);
             this.WGSdec.Name = "WGSdec";
             this.WGSdec.Size = new System.Drawing.Size(93, 21);
             this.WGSdec.TabIndex = 11;
@@ -136,8 +123,10 @@ namespace Data
             // WGSnov
             // 
             this.WGSnov.AutoSize = true;
+            this.WGSnov.Checked = true;
+            this.WGSnov.CheckState = System.Windows.Forms.CheckState.Checked;
             this.WGSnov.Location = new System.Drawing.Point(269, 96);
-            this.WGSnov.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.WGSnov.Margin = new System.Windows.Forms.Padding(4);
             this.WGSnov.Name = "WGSnov";
             this.WGSnov.Size = new System.Drawing.Size(93, 21);
             this.WGSnov.TabIndex = 10;
@@ -147,8 +136,10 @@ namespace Data
             // WGSokt
             // 
             this.WGSokt.AutoSize = true;
+            this.WGSokt.Checked = true;
+            this.WGSokt.CheckState = System.Windows.Forms.CheckState.Checked;
             this.WGSokt.Location = new System.Drawing.Point(155, 96);
-            this.WGSokt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.WGSokt.Margin = new System.Windows.Forms.Padding(4);
             this.WGSokt.Name = "WGSokt";
             this.WGSokt.Size = new System.Drawing.Size(78, 21);
             this.WGSokt.TabIndex = 9;
@@ -158,8 +149,10 @@ namespace Data
             // WGSsep
             // 
             this.WGSsep.AutoSize = true;
+            this.WGSsep.Checked = true;
+            this.WGSsep.CheckState = System.Windows.Forms.CheckState.Checked;
             this.WGSsep.Location = new System.Drawing.Point(40, 96);
-            this.WGSsep.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.WGSsep.Margin = new System.Windows.Forms.Padding(4);
             this.WGSsep.Name = "WGSsep";
             this.WGSsep.Size = new System.Drawing.Size(97, 21);
             this.WGSsep.TabIndex = 8;
@@ -169,8 +162,10 @@ namespace Data
             // WGSaug
             // 
             this.WGSaug.AutoSize = true;
+            this.WGSaug.Checked = true;
+            this.WGSaug.CheckState = System.Windows.Forms.CheckState.Checked;
             this.WGSaug.Location = new System.Drawing.Point(384, 66);
-            this.WGSaug.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.WGSaug.Margin = new System.Windows.Forms.Padding(4);
             this.WGSaug.Name = "WGSaug";
             this.WGSaug.Size = new System.Drawing.Size(88, 21);
             this.WGSaug.TabIndex = 7;
@@ -180,8 +175,10 @@ namespace Data
             // WGSjul
             // 
             this.WGSjul.AutoSize = true;
+            this.WGSjul.Checked = true;
+            this.WGSjul.CheckState = System.Windows.Forms.CheckState.Checked;
             this.WGSjul.Location = new System.Drawing.Point(269, 66);
-            this.WGSjul.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.WGSjul.Margin = new System.Windows.Forms.Padding(4);
             this.WGSjul.Name = "WGSjul";
             this.WGSjul.Size = new System.Drawing.Size(47, 21);
             this.WGSjul.TabIndex = 6;
@@ -191,8 +188,10 @@ namespace Data
             // WGSjun
             // 
             this.WGSjun.AutoSize = true;
+            this.WGSjun.Checked = true;
+            this.WGSjun.CheckState = System.Windows.Forms.CheckState.Checked;
             this.WGSjun.Location = new System.Drawing.Point(155, 66);
-            this.WGSjun.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.WGSjun.Margin = new System.Windows.Forms.Padding(4);
             this.WGSjun.Name = "WGSjun";
             this.WGSjun.Size = new System.Drawing.Size(52, 21);
             this.WGSjun.TabIndex = 5;
@@ -202,8 +201,10 @@ namespace Data
             // WGSmei
             // 
             this.WGSmei.AutoSize = true;
+            this.WGSmei.Checked = true;
+            this.WGSmei.CheckState = System.Windows.Forms.CheckState.Checked;
             this.WGSmei.Location = new System.Drawing.Point(40, 66);
-            this.WGSmei.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.WGSmei.Margin = new System.Windows.Forms.Padding(4);
             this.WGSmei.Name = "WGSmei";
             this.WGSmei.Size = new System.Drawing.Size(52, 21);
             this.WGSmei.TabIndex = 4;
@@ -213,8 +214,10 @@ namespace Data
             // WGSapr
             // 
             this.WGSapr.AutoSize = true;
+            this.WGSapr.Checked = true;
+            this.WGSapr.CheckState = System.Windows.Forms.CheckState.Checked;
             this.WGSapr.Location = new System.Drawing.Point(384, 37);
-            this.WGSapr.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.WGSapr.Margin = new System.Windows.Forms.Padding(4);
             this.WGSapr.Name = "WGSapr";
             this.WGSapr.Size = new System.Drawing.Size(57, 21);
             this.WGSapr.TabIndex = 3;
@@ -224,8 +227,10 @@ namespace Data
             // WGSmrt
             // 
             this.WGSmrt.AutoSize = true;
+            this.WGSmrt.Checked = true;
+            this.WGSmrt.CheckState = System.Windows.Forms.CheckState.Checked;
             this.WGSmrt.Location = new System.Drawing.Point(269, 37);
-            this.WGSmrt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.WGSmrt.Margin = new System.Windows.Forms.Padding(4);
             this.WGSmrt.Name = "WGSmrt";
             this.WGSmrt.Size = new System.Drawing.Size(66, 21);
             this.WGSmrt.TabIndex = 2;
@@ -235,8 +240,10 @@ namespace Data
             // WGSfeb
             // 
             this.WGSfeb.AutoSize = true;
+            this.WGSfeb.Checked = true;
+            this.WGSfeb.CheckState = System.Windows.Forms.CheckState.Checked;
             this.WGSfeb.Location = new System.Drawing.Point(155, 37);
-            this.WGSfeb.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.WGSfeb.Margin = new System.Windows.Forms.Padding(4);
             this.WGSfeb.Name = "WGSfeb";
             this.WGSfeb.Size = new System.Drawing.Size(79, 21);
             this.WGSfeb.TabIndex = 1;
@@ -246,8 +253,10 @@ namespace Data
             // WGSjan
             // 
             this.WGSjan.AutoSize = true;
+            this.WGSjan.Checked = true;
+            this.WGSjan.CheckState = System.Windows.Forms.CheckState.Checked;
             this.WGSjan.Location = new System.Drawing.Point(40, 37);
-            this.WGSjan.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.WGSjan.Margin = new System.Windows.Forms.Padding(4);
             this.WGSjan.Name = "WGSjan";
             this.WGSjan.Size = new System.Drawing.Size(73, 21);
             this.WGSjan.TabIndex = 0;
@@ -258,7 +267,7 @@ namespace Data
             // 
             this.WGSinleiding.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.WGSinleiding.Location = new System.Drawing.Point(77, 231);
-            this.WGSinleiding.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.WGSinleiding.Margin = new System.Windows.Forms.Padding(4);
             this.WGSinleiding.Multiline = true;
             this.WGSinleiding.Name = "WGSinleiding";
             this.WGSinleiding.ReadOnly = true;
@@ -270,18 +279,17 @@ namespace Data
             // 
             this.WGStitel.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.WGStitel.Location = new System.Drawing.Point(91, 10);
-            this.WGStitel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.WGStitel.Margin = new System.Windows.Forms.Padding(4);
             this.WGStitel.Name = "WGStitel";
             this.WGStitel.Size = new System.Drawing.Size(1279, 53);
             this.WGStitel.TabIndex = 28;
             this.WGStitel.Text = "Temperatuur en fiets diefstal\r\n";
             this.WGStitel.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.WGStitel.TextChanged += new System.EventHandler(this.WGStitel_TextChanged);
             // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(1011, 705);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(167, 31);
             this.button1.TabIndex = 26;
@@ -300,7 +308,7 @@ namespace Data
             this.Controls.Add(this.WGStitel);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.myButton);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Datavisualisatie1";
             this.Text = "dataVisualisatie1";
             this.Load += new System.EventHandler(this.myButton_Click);
